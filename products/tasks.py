a35b7047f59c5cf6343ca916fc7c469c4688e1e7
ismail <ismailrazak009@gmail.com>
2025-04-04 20:10:02 +0530

    Improve gemini prompt for ai reviews

--- products/tasks.py
+++ products/tasks.py
@@ -28,14 +28,14 @@ def ai_summary_review_task(product_id):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=f"""
-        Your job is to summarize reviews for a product based on the reviews given to you in a formal and simple to understand way.
-        summarize those reviews into 5 lines while maintaining a proper balance between the pros and cons of the product.
-        you will given a list of reviews and when possible a ai summary review. analyze both of these and generate a honest non-biased review while maintaing factual accuracy.
+        Your task is to summarize an abstract into 5 sentences at most. 
+        Avoid technical jargon and explain it in the simplest of words.
+        summarize those reviews into 5 lines while  trying to maintain a proper balance between the pros and cons of the product.If either pros or cons dont exist,move on with summarizing it.
+        you will be given a list of reviews and when possible a ai summary review. analyze both of these and generate a honest non-biased review while maintaining factual accuracy.
+        Your Response will only contain words about the review and nothing else.
+        If no postive or negative reviews or there is nothing of factual knowledge or useful response to return, return "".
         Ai Review:{product.ai_review}
         list of reviews:{all_descriptions}
-        
-        Example:
-        Customers appreciate the phone's camera quality, speed, and display. They find the main camera quality superb in day light, with good charging speed. The high refresh rate makes the animations very smooth. The apps open instantly and the higher refresh rate makes the animation smoother. The phone looks classy and has a premium design. The battery life is long-lasting, and the AI feature is also very good. Overall, customers are satisfied with the performance, display quality, battery life, and overall functionality of the phone.
         """,
     )
     product.ai_review = response.text
